1ff6bc79d21ff9d1da9da0588276c1873fed2077
sikivpn <147810890+sikivpn@users.noreply.github.com>
2023-10-27 23:16:39 +0700

    Add files via upload

--- xlpy/__init__.py
+++ xlpy/__init__.py
@@ -3,8 +3,8 @@ from .client import XL
 
 __copyright__       = 'Copyright 2018 by Anggi Alberto'
 __version__         = '3.0.0'
-__author__          = 'Anggi Alberto'
-__author_email__    = 'albertoanggi@gmail.com'
-__url__             = 'http://github.com/albertoanggi/xl-py'
+__author__          = 'sikivpn allow'
+__author_email__    = 'sikihuut@gmail.com'
+__url__             = 'http://github.com/sikivpn/xl-py'
 
 __all__ = ['Config','XL']
